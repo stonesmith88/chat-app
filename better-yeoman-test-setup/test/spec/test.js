d42a2a004b557ba0585f1497719e598ec5adfbd4
stone smith <stonesmith88@yahoo.com>
2014-06-02 09:07:38 -0400

    still need to work out quirks

--- better-yeoman-test-setup/test/spec/test.js
+++ better-yeoman-test-setup/test/spec/test.js
@@ -6,8 +6,7 @@
     describe('Give it some context', function () {
         describe('maybe a bit more context here', function () {
             it('should run here few assertions', function () {
-              var answer = 2;
-              expect(2).to.equal(answer);
+
             });
         });
     });
